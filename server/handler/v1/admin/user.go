1f4127c6401931fdfaa51f8f7f90c295a3b07e27
zhangyunfei1 <zhangyunfei1@100tal.com>
2021-07-24 07:53:09 +0800

    整理代码

---
+++ server/handler/v1/admin/user.go
@@ -0,0 +1,184 @@
+package admin
+
+import (
+	"project/dto/request"
+	"project/dto/response"
+	"project/handler/middleware"
+	"project/model/system"
+	"project/service"
+	"project/utils"
+	"project/zvar"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type userHandler struct {
+	userService *service.UserService
+}
+
+func NewUserHandler() *userHandler {
+	return &userHandler{
+		userService: &service.UserService{},
+	}
+}
+
+func (h *userHandler) Router(router *gin.RouterGroup) {
+	apiRouter := router.Group("user").Use(middleware.OperationRecord())
+	{
+		apiRouter.POST("register", h.register)             // 用户注册账号
+		apiRouter.POST("changePassword", h.changePassword) // 用户修改密码
+		apiRouter.POST("list", h.list)                     // 分页获取用户列表
+		apiRouter.POST("setRole", h.setRole)               // 设置用户权限
+		apiRouter.DELETE("delete", h.delete)               // 删除用户
+		apiRouter.PUT("update", h.update)                  // 设置用户信息
+	}
+}
+
+// @Tags User
+// @Summary 用户注册账号
+// @Produce  application/json
+// @Param data body request.Register true "用户名, 昵称, 密码, 角色ID"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"注册成功"}"
+// @Router /user/register [post]
+func (h *userHandler) register(c *gin.Context) {
+	var r request.Register
+	_ = c.ShouldBindJSON(&r)
+	if err := utils.Verify(r, utils.RegisterVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	user := &system.User{Username: r.Username, NickName: r.NickName, Password: r.Password, HeaderImg: r.HeaderImg, AuthorityId: r.AuthorityId}
+	err, userReturn := h.userService.Register(*user)
+	if err != nil {
+		zvar.Log.Error("注册失败!", zap.Any("err", err))
+		response.FailWithDetailed(response.UserResponse{User: userReturn}, "注册失败", c)
+	} else {
+		response.OkWithDetailed(response.UserResponse{User: userReturn}, "注册成功", c)
+	}
+}
+
+// @Tags User
+// @Summary 用户修改密码
+// @Security ApiKeyAuth
+// @Produce  application/json
+// @Param data body request.ChangePasswordStruct true "用户名, 原密码, 新密码"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"修改成功"}"
+// @Router /user/changePassword [put]
+func (h *userHandler) changePassword(c *gin.Context) {
+	var user request.ChangePasswordStruct
+	_ = c.ShouldBindJSON(&user)
+	if err := utils.Verify(user, utils.ChangePasswordVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	u := &system.User{Username: user.Username, Password: user.Password}
+	if err, _ := h.userService.ChangePassword(u, user.NewPassword); err != nil {
+		zvar.Log.Error("修改失败!", zap.Any("err", err))
+		response.FailWithMessage("修改失败，原密码与当前账户不符", c)
+	} else {
+		response.OkWithMessage("修改成功", c)
+	}
+}
+
+// @Tags User
+// @Summary 分页获取用户列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.PageInfo true "页码, 每页大小"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /user/list [post]
+func (h *userHandler) list(c *gin.Context) {
+	var pageInfo request.PageInfo
+	_ = c.ShouldBindJSON(&pageInfo)
+	if err := utils.Verify(pageInfo, utils.PageInfoVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	if err, list, total := h.userService.GetUserInfoList(pageInfo); err != nil {
+		zvar.Log.Error("获取失败!", zap.Any("err", err))
+		response.FailWithMessage("获取失败", c)
+	} else {
+		response.OkWithDetailed(response.PageResult{
+			List:     list,
+			Total:    total,
+			Page:     pageInfo.Page,
+			PageSize: pageInfo.PageSize,
+		}, "获取成功", c)
+	}
+}
+
+// @Tags User
+// @Summary 设置用户权限
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.SetUserAuth true "用户UUID, 角色ID"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"修改成功"}"
+// @Router /user/setRole [post]
+func (h *userHandler) setRole(c *gin.Context) {
+	var sua request.SetUserAuth
+	_ = c.ShouldBindJSON(&sua)
+	if UserVerifyErr := utils.Verify(sua, utils.SetUserAuthorityVerify); UserVerifyErr != nil {
+		response.FailWithMessage(UserVerifyErr.Error(), c)
+		return
+	}
+	if err := h.userService.SetUserAuthority(sua.UUID, sua.AuthorityId); err != nil {
+		zvar.Log.Error("修改失败!", zap.Any("err", err))
+		response.FailWithMessage("修改失败", c)
+	} else {
+		response.OkWithMessage("修改成功", c)
+	}
+}
+
+// @Tags User
+// @Summary 删除用户
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.GetById true "用户ID"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Router /user/delete [delete]
+func (h *userHandler) delete(c *gin.Context) {
+	var reqId request.GetById
+	_ = c.ShouldBindJSON(&reqId)
+	if err := utils.Verify(reqId, utils.IdVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	jwtId := utils.GetUserID(c)
+	if jwtId == uint(reqId.ID) {
+		response.FailWithMessage("删除失败, 自杀失败", c)
+		return
+	}
+	if err := h.userService.DeleteUser(reqId.ID); err != nil {
+		zvar.Log.Error("删除失败!", zap.Any("err", err))
+		response.FailWithMessage("删除失败", c)
+	} else {
+		response.OkWithMessage("删除成功", c)
+	}
+}
+
+// @Tags User
+// @Summary 设置用户信息
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body system.User true "ID, 用户名, 昵称, 头像链接"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"设置成功"}"
+// @Router /user/update [put]
+func (h *userHandler) update(c *gin.Context) {
+	var user system.User
+	_ = c.ShouldBindJSON(&user)
+	if err := utils.Verify(user, utils.IdVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	if err, ReqUser := h.userService.SetUserInfo(user); err != nil {
+		zvar.Log.Error("设置失败!", zap.Any("err", err))
+		response.FailWithMessage("设置失败", c)
+	} else {
+		response.OkWithDetailed(gin.H{"userInfo": ReqUser}, "设置成功", c)
+	}
+}
